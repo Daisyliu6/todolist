7205bdf0874c4a2321ce8dcd4a81b7bd9118162a
daisy <zacaopingzi@gmail.com>
2020-06-15 19:14:17 +1000

    Add gitignore, Profile and update ports

--- app.js
+++ app.js
@@ -115,10 +115,11 @@ app.post("/delete", function (req, res) {
   }
 });
 
-app.post("/work", function (req, res) {
-  res.redirect("/work");
-});
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
-app.listen(3000, function () {
+app.listen(port, function () {
   console.log("Server started successfully!");
 });
